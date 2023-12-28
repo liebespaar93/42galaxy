69c2ab68fac2a83308ec8eaa968974c79424e5c3
kyoulee <kyoulee@backsudogoui-MacBookAir.local>
2023-12-28 18:29:59 +0900

    update Area element to useful

--- src/app/space/page.tsx
+++ src/app/space/page.tsx
@@ -1,32 +1,107 @@
 "use client"
 
 import * as THREE from 'three'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import { MutableRefObject, Suspense, useRef } from 'react'
+import React, { MutableRefObject, ReactElement, Suspense, isValidElement, useRef } from 'react'
 
 import { OrbitControls as OrbitControlsType } from 'three-stdlib'
 
-import MyPlant from '../ui/canvas/MyPlant'
+import MyPlant, { PlantProps } from '../ui/canvas/MyPlant'
 import MySpace from '../ui/canvas/MySpace'
 import MyGrid from '../ui/canvas/MyGrid'
-import { preload } from 'react-dom'
+
+const __DEBUG__ = false
 
 type GalaxyProps = {
     orbitRef: MutableRefObject<OrbitControlsType | null>
 }
 
-function Galaxy({ orbitRef }: GalaxyProps) {
+
+type AreaProps = {
+    children: ReactElement<PlantProps | AreaProps> | ReactElement<PlantProps | AreaProps>[]
+    level: number
+    position?: THREE.Vector3 | undefined
+    orbit_radius?: number | undefined
+}
+
+/**
+ * 
+ * @param param
+ * @returns 
+ * @memo isValidElement React element 인지 확인
+ * @tip array 는 React element 아니다
+ */
+function Area({ position, level, children }: AreaProps) {
+    if (__DEBUG__)
+        console.log("Area")
+
+    if (isValidElement(children))
+        children = [children]
+
+    const orbit: ReactElement<PlantProps | AreaProps>[] = [];
+
+    children.map((value, index) => {
+        orbit.push(
+            <group key={index} rotation={[0, value.props.orbit_radius ? -value.props.orbit_radius : 0, 0]}>
+                <group position={[0, 0, -level * 10]}>
+                    {value}
+                </group>
+            </group>
+        )
+    })
+
     return (
-        <>
-            <MyGrid size={10} position={[0, 0, 0]} />
-            <MyPlant orbitRef={orbitRef} file={"/assets/galaxy/cube_plant/cube_plant.gltf"} name={"Libft Plant"}
-                position={[1, 1, 1]} rotation={[0, 0, 0]} />
-        </>
+        <group>
+            <mesh position={position} rotation={[Math.PI * 0.5, 0, 0]} scale={[level * 10, level * 10, level * 10]}>
+                <ringGeometry args={[1, 1.01, 64]} />
+                <meshNormalMaterial side={THREE.DoubleSide} />
+            </mesh>
+            {orbit}
+        </group>
     )
 }
 
-function CanvasSet() {
+function Galaxy({ orbitRef }: GalaxyProps) {
+    if (__DEBUG__)
+        console.log("Galaxy")
+    return (
+        <group>
+            <MyGrid size={10} position={[0, 0, 0]} />
+            <Area level={0.1}>
+                <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 0.3} file={"/assets/galaxy/cube_plant/cube_plant.gltf"} name={"Libft"}
+                    rotation={[0, 0, 0]} />
+            </Area>
+            <Area level={1}>
+                <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 0.33} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"ft_printf"}/>
+                <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 1.0} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"get_next_line"}/>
+                <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 1.66} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"Born2beroot"}/>
+            </Area>
+            <Area level={2}>
+                <Area level={0.5} orbit_radius={Math.PI * 0.3}>
+                    <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 0.0} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"minitalk"} />
+                    <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 1.0} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"pipex"} />
+                </Area>
+                <Area level={0.5} orbit_radius={Math.PI * 0.6}>
+                    <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 0.33} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"so_long"} />
+                    <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 1.0} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"fdf"} />
+                    <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 1.66} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"fract-ol"} />
+                </Area>
+                <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 1.8} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"push_swap"} />
+                <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 1.0} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"Exam Rank 02"} />
+            </Area>
+            <Area level={3}>
+                <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 0.0} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"Exam Rank 03"} />
+                <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 0.66} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"minishell"} />
+                <MyPlant orbitRef={orbitRef} orbit_radius={Math.PI * 1.33} file={"/assets/galaxy/untitled_plant/untitled_plant.gltf"} name={"phliosophers"} />
+            </Area>
+        </group>
+    )
+}
+
+function SpaceSetting() {
+    if (__DEBUG__)
+        console.log("SpaceSetting")
     const renderer = new THREE.WebGLRenderer();
     const { camera } = useThree()
     const orbitRef = useRef<OrbitControlsType>(new OrbitControlsType(camera, renderer.domElement))
@@ -35,17 +110,19 @@ function CanvasSet() {
         <>
             <OrbitControls ref={orbitRef} />
             <Suspense fallback={<text>Loading</text>} />
-            <MySpace file="/assets/space/nebula.hdr" />
             <Galaxy orbitRef={orbitRef} />
+            <MySpace file="/assets/space/nebula.hdr" />
         </>
     )
 }
 
 export default function Space() {
+    if (__DEBUG__)
+        console.log("Space")
     return (
         <div className="h-full w-full">
             <Canvas className="h-full w-full">
-                <CanvasSet />
+                <SpaceSetting />
             </Canvas>
         </div>
     )
--- src/app/ui/canvas/MyPlant.tsx
+++ src/app/ui/canvas/MyPlant.tsx
@@ -2,47 +2,53 @@
 import { MutableRefObject, useState } from 'react'
 import * as THREE from 'three'
 import { GLTF } from 'three-stdlib'
-import { ObjectMap, ThreeEvent, useThree } from '@react-three/fiber'
-import { useGLTF } from '@react-three/drei'
+import { ObjectMap, ThreeEvent } from '@react-three/fiber'
+import { Clone, useGLTF } from '@react-three/drei'
 
 import type { OrbitControls as OrbitControlsType } from 'three-stdlib'
 
+const __DEBUG__ = false
 
-type PlantProps = {
-    orbitRef : MutableRefObject<OrbitControlsType | null>
+export type PlantProps = {
+    orbitRef: MutableRefObject<OrbitControlsType | null>
     file: string
-    name: string
-    position: [number, number, number] | THREE.Vector3 | undefined
-    rotation: [number, number, number] | THREE.Euler | undefined
+    name?: string | undefined
+    orbit_radius?: number | undefined
+    position?: THREE.Vector3 | undefined
+    rotation?: [number, number, number] | THREE.Euler | undefined
 }
 
+/**
+ * @param GLTFProps 
+ * @returns 
+ * @memo primitive 는 동일한 오브잭트 한번만 사용가능
+ * @meno Clone 는 동일한 오브잭트 여러번 사용가능
+ */
 function MyGLTF({ file }: GLTFProps) {
     const gltf: (GLTF & ObjectMap) | (GLTF & ObjectMap)[] = useGLTF(file);
-    console.log(file);
+
     if (!gltf) // 에러처리 행성 필요 필요
         return (<></>);
     return (
-        <primitive object={gltf.scene}></primitive>
+        <>
+            <Clone object={gltf.scene} />
+        </>
     )
 }
 
-
-function zoom_lerp() {
-
-}
-
-function MyPlant({ orbitRef, file, position, rotation }: PlantProps) {
-    const target = new THREE.Vector3;
-    const calculate = new THREE.Vector3(0, 0, 0);
-
+function MyPlant({ orbitRef, file, name, position, rotation }: PlantProps) {
+    if (__DEBUG__)
+        console.log("MyPlant")
 
     function plant_info(event: ThreeEvent<MouseEvent>) {
+        const target = new THREE.Vector3;
+        const calculate = new THREE.Vector3(0, 0, 0);
+
         event.stopPropagation();
         event.eventObject.getWorldPosition(target);
 
         // 나중에 lerp 하게 움직이는 카메라 필요
-        if (orbitRef && orbitRef.current)
-        {
+        if (orbitRef && orbitRef.current) {
             calculate.set(0, 0, 0);
             calculate.add(orbitRef.current.object.position).sub(target)
             calculate.normalize().multiplyScalar(5).add(target)
@@ -53,13 +59,9 @@ function MyPlant({ orbitRef, file, position, rotation }: PlantProps) {
     }
 
     return (
-        <>
-            <group onClick={plant_info} position={position} rotation={rotation}>
-                <MyGLTF file={file} />
-            </group>
-        </>
-    )
+        <group onClick={plant_info} >
+            <MyGLTF file={file} />
+        </group>)
 }
-
 
 export default MyPlant
